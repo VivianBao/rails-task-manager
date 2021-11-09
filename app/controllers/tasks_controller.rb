bd7c58a7030d6dad8448a76cc755c5a6b1b1ce79
Vivian Bow <vivianbao1009@gmail.com>
2021-11-09 15:58:01 +0900

    Challenge 2 first draft

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,45 @@
+require_relative '../models/task'
+
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path
+    else
+      render new_task_path
+    end
+  end
+
+  def update
+    Task.find(params[:id]).update(task_params)
+    redirect_to task_path
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
+end
