5a9f0190afcd6cdabb396c9e16cc85a380dd9ae9
Vivian Bow <vivianbao1009@gmail.com>
2022-02-10 22:19:59 +0900

    Added new action to prevent page refresh

--- app/javascript/controllers/submit_checkbox_controller.js
+++ app/javascript/controllers/submit_checkbox_controller.js
@@ -1,27 +1,34 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets=['form','checkbox']
+  static targets=['form','checkbox', 'taskContainer', 'task']
   connect() {
     console.log("Hello from our first Stimulus controller")
   }
 
-  submitHello() {
+  submitForm() {
     this.formTarget.submit();
-  // Rails.ajax({
-  //   type: "patch",
-  //   url: `${this.formTarget.action}`,
-  //   data: JSON.stringify({
-  //     "task": { "completed": this.checkboxTarget.checked}
-  //   }),
-  //   dataType: "json",
-  //   contentType: "application/json",
-  //   success: function (result) {
-  //     formTarget.submit();
-  //   },
-  //   error: function (e) {
-  //     console.log(e);
-  //   },
-  // })
+    console.log("submitForm Ran")
+  }
+
+  stopRefresh() {
+    event.preventDefault()
+    console.log("stopRefresh ran")
   }
 }
+
+// Rails.ajax({
+//   type: "patch",
+//   url: `${this.formTarget.action}`,
+//   data: JSON.stringify({
+//     "task": { "completed": this.checkboxTarget.checked}
+//   }),
+//   dataType: "json",
+//   contentType: "application/json",
+//   success: function (result) {
+//     formTarget.submit();
+//   },
+//   error: function (e) {
+//     console.log(e);
+//   },
+// })
